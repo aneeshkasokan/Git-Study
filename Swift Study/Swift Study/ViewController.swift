8a3e2f544f9ce9731814d2656982734f73c5c4e4
ARUN <arun@fs-mac-316.local>
2018-04-28 10:12:14 +0530

    New Project Added

---
+++ Swift Study/Swift Study/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift Study
+//
+//  Created by ARUN on 28/04/18.
+//  Copyright © 2018 ARUN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
